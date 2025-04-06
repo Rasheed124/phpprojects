b8ac857c4862736375dbffb59018c05d620c8a89
Rasheed124 <rashdevatrealglobe@gmail.com>
2025-04-06 22:54:53 +0100

    discover page pushed

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const toggleBtn = document.getElementById("sidebarToggle");
+const sidebar = document.getElementById("sidebar");
+//   const toggleBtn = document.getElementById("toggleSidebar");
+
+toggleBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("collapsed");
+});
